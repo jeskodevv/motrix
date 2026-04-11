37e52b0faf7d1edeb6463ecf8e0b4b0192915f69
jesko <jeskodevv@gmail.com>
2026-04-11 23:32:38 +0500

    Update motrix.cpp with motor control functions

--- src/library/motrix.cpp
+++ src/library/motrix.cpp
@@ -2,60 +2,60 @@
 #include "motrix.h"
 
 void motrix::motorPower(char unit, int power){
-	_U = unit;
-	_P = power;
-	if(_U == 'A'){
-		if(_P>0 && _P<=255){
-			analogWrite(5, abs(_P));
-			digitalWrite(3, 1);
-		}else if(_P<0 && _P>=-255){
-			analogWrite(5, abs(_P));
-			digitalWrite(4, 1);
-		}else{
-			analogWrite(5, 0);
-			digitalWrite(4, 0);
-			digitalWrite(3, 0);
-		}
-	}else if(_U == 'B'){
-		if(_P>0 && _P<=255){
-			analogWrite(6, abs(_P));
-			digitalWrite(8, 1);
-		}else if(_P<0 && _P>=-255){
-			analogWrite(6, abs(_P));
-			digitalWrite(7, 1);
-		}else{
-			analogWrite(6, 0);
-			digitalWrite(7, 0);
-			digitalWrite(8, 0);
-		}
-	}
+\t_U = unit;
+\t_P = power;
+\tif(_U == 'A'){
+\t\tif(_P>0 && _P<=255){
+\t\t\tanalogWrite(5, abs(_P));
+\t\t\tdigitalWrite(3, 1);
+\t\t}else if(_P<0 && _P>=-255){
+\t\t\tanalogWrite(5, abs(_P));
+\t\t\tdigitalWrite(4, 1);
+\t\t}else{
+\t\t\tanalogWrite(5, 0);
+\t\t\tdigitalWrite(4, 0);
+\t\t\tdigitalWrite(3, 0);
+\t\t}
+\t}else if(_U == 'B'){
+\t\tif(_P>0 && _P<=255){
+\t\t\tanalogWrite(6, abs(_P));
+\t\t\tdigitalWrite(8, 1);
+\t\t}else if(_P<0 && _P>=-255){
+\t\t\tanalogWrite(6, abs(_P));
+\t\t\tdigitalWrite(7, 1);
+\t\t}else{
+\t\t\tanalogWrite(6, 0);
+\t\t\tdigitalWrite(7, 0);
+\t\t\tdigitalWrite(8, 0);
+\t\t}
+\t}
 }
 
-void motrix::motorSteer(int steering, power){
-	_S = steering;
-	_P = power;
-	if(_S >= 255){
-		motrix::motorPower(A, _P);
-		motrix::motorPower(B, _P*-1);
-	}else if(_S <= -255){
-		motrix::motorPower(A, _P*-1);
-		motrix::motorPower(B, _P);
-	}else{
-		motrix::motorPower(A, _P*(255+_S)/255);
-		motrix::motorPower(B, _P*(255-_S)/255);
-	}
+void motrix::motorSteer(int steering, int power){
+\t_S = steering;
+\t_P = power;
+\tif(_S >= 255){
+\tmotrix::motorPower('A', _P);
+\tmotrix::motorPower('B', _P*-1);
+\t}else if(_S <= -255){
+\tmotrix::motorPower('A', _P*-1);
+\tmotrix::motorPower('B', _P);
+\t}else{
+\tmotrix::motorPower('A', _P*(255+_S)/255);
+\tmotrix::motorPower('B', _P*(255-_S)/255);
+\t}
 }
 
-void motrix::motorTank(int power_a, power_b){
-	_PA = power_a;
-	_PB = power_b;
-	motrix::motorPower(A, _PA);
-	motrix::motorPower(B, _PB);
+void motrix::motorTank(int power_a, int power_b){
+\t_PA = power_a;
+\t_PB = power_b;
+\tmotrix::motorPower('A', _PA);
+\tmotrix::motorPower('B', _PB);
 }
 
 void motrix::debug(int delay){
-	_D = delay;
-	digitalWrite(13, 1);
-	delay(_D*1000);
-	digitalWrite(13, 0);
+\t_D = delay;
+\tdigitalWrite(13, 1);
+\tdelay(_D*1000);
+\tdigitalWrite(13, 0);
 }
